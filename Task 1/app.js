5d5e4e05b3ac0879a5252672217594ece6c29328
GreatKarlie <62343109+GreatKarlie@users.noreply.github.com>
2021-05-21 14:28:22 +0200

    Add files via upload

---
+++ Task 1/app.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-bars");
+const nav =document.querySelector("nav ul");
+function sideMenu(){
+    nav.classList.toggle("slide");
+}
+
+/*Large-scale project management
+Materials Development
+                    
+Educational consultation across the spectrum from family literacy programmes, enhancing language and 
+communication in primary school children, project management of large scale AET training interventions, 
+teaching AET and other learning interventions to illiterate adults, AET and bridging training programmes for youth and adults*/
